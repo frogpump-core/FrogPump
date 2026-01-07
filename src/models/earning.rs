002520d86b6476e9c68c1a3593a3085142d60a00
frogpump-core <frogpump2026@gmail.com>
2026-01-07 15:15:00 +0900

    Add Earning struct definition

---
+++ src/models/earning.rs
@@ -0,0 +1,33 @@
+use std::fmt;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Earning {
+    pub id: String,
+    pub token_id: String,
+    pub agent_id: String,
+    pub amount: f64,
+    pub claimed: bool,
+    pub claimed_at: Option<String>,
+    pub created_at: String,
+}
+
+impl fmt::Display for Earning {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let status = if self.claimed {
+            "claimed"
+        } else {
+            "unclaimed"
+        };
+        write!(
+            f,
+            "Earning {} [{}] - {} ({})",
+            self.id,
+            self.token_id,
+            self.format_amount(),
+            status
+        )
+    }
+}
+
